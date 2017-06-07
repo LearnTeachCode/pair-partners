1e8bcfa17a6fdf1a9c4f3c58fa1d79dc8630d88b
LearningNerd <learningnerd@gmail.com>
2017-06-07 11:50:40 -0700

    Display data from Firebase on web page

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -9,3 +9,14 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+// Create a JavaScript object for the HTML element that has id="message"
+var messageBox = document.getElementById("message");
+
+// Get a reference to the row of our database called "greeting"
+var dbRef = firebase.database().ref().child("greeting");
+
+// Whenever our database reference is updated, show the data on our web page!
+dbRef.on("value", function(dataSnapshot) {
+	messageBox.textContent = dataSnapshot.val();
+});
