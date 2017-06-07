f0eb1c819929a6e692241d95a6107377f3b8c668
LearningNerd <learningnerd@gmail.com>
2017-06-07 11:49:49 -0700

    Add Firebase initialization code to JS file

---
+++ firebase-test/firebasetest.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCdoBkKYmhVlgvy46sBKLLknJUszf-TYAc",
+	authDomain: "test-web-app-4fde9.firebaseapp.com",
+	databaseURL: "https://test-web-app-4fde9.firebaseio.com",
+	projectId: "test-web-app-4fde9",
+	storageBucket: "test-web-app-4fde9.appspot.com",
+	messagingSenderId: "406177608219"
+};
+
+firebase.initializeApp(config);
