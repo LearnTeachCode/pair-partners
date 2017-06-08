c0850a5e35eb01d28ddf8c9b428905e86f19a96f
LearningNerd <learningnerd@gmail.com>
2017-06-08 13:33:18 -0700

    Display myname from Firebase

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -13,8 +13,8 @@ firebase.initializeApp(config);
 // Create a JavaScript object for the HTML element that has id="message"
 var messageBox = document.getElementById("message");
 
-// Get a reference to the row of our database called "greeting"
-var dbRef = firebase.database().ref().child("greeting");
+// Get a reference to the row of our database called "myname"
+var dbRef = firebase.database().ref().child("myname");
 
 // Whenever our database reference is updated, show the data on our web page!
 dbRef.on("value", function(dataSnapshot) {
