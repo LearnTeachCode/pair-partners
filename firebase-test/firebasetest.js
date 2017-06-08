04f4486e1c952bbda6bb4842144d18190fe5a661
r7uaz0n <rcktzn007@gmail.com>
2017-06-07 17:15:19 -0700

    firebasetest.js commit

---
+++ firebase-test/firebasetest.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBHaZrBCDS_LPBUZvqbWg3-s1er_JqtUSY",
+    authDomain: "fir-test-4f28e.firebaseapp.com",
+    databaseURL: "https://fir-test-4f28e.firebaseio.com",
+    projectId: "fir-test-4f28e",
+    storageBucket: "fir-test-4f28e.appspot.com",
+    messagingSenderId: "276366659662"
+  };
+  firebase.initializeApp(config);
