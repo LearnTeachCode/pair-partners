c070d6590a895783a80b2f7782a05aed2ac52e0d
ssiarules <weadmin@Whitney-Aluyon.local>
2017-06-08 10:15:08 -0700

    firebasetest.js

---
+++ firebase-test/firebasetest.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCURkSNkiFsXyD9A3VF4da2rs-MsHeMbjE",
+   authDomain: "fir-test-cd2a4.firebaseapp.com",
+   databaseURL: "https://fir-test-cd2a4.firebaseio.com",
+   projectId: "fir-test-cd2a4",
+   storageBucket: "fir-test-cd2a4.appspot.com",
+   messagingSenderId: "382326280146"
+ };
+ firebase.initializeApp(config);
