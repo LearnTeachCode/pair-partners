98a98612487a236d0d26cd8c93e179b203cc98fc
kammitama5 <krystal.maughan@gmail.com>
2017-06-08 01:24:29 +0000

    initialize firebase

---
+++ firebase-test/firebasetest.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyArtK1XCgIvcyRC8-SVTdRn3bsSKA-ogNo",
+    authDomain: "fir-test-6d238.firebaseapp.com",
+    databaseURL: "https://fir-test-6d238.firebaseio.com",
+    projectId: "fir-test-6d238",
+    storageBucket: "fir-test-6d238.appspot.com",
+    messagingSenderId: "64289804527"
+  };
+  
+  firebase.initializeApp(config);
