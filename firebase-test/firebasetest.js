0631c0baa29348795fd4c2092bd3e522993f94b3
r7uaz0n <rcktzn007@gmail.com>
2017-06-10 13:37:06 -0700

    testing firebase using Firebase Console

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -7,4 +7,16 @@
     storageBucket: "fir-test-4f28e.appspot.com",
     messagingSenderId: "276366659662"
   };
+  
   firebase.initializeApp(config);
+
+// Create a JavaScript object for the HTML element that has id="message"
+var messageBox = document.getElementById("message");
+
+// Get a reference to the row of our database called "greeting"
+var dbRef = firebase.database().ref().child("greeting");
+
+// Whenever our database reference is updated, show the data on our web page!
+dbRef.on("value", function(dataSnapshot) { 
+  messageBox.textContent = dataSnapshot.val(); 
+});
