9bd190103aaf840b4cf37e82eccd357a01cd6414
Melanie Mendavia <mlmendavia@gmail.com>
2017-06-09 21:53:26 -0700

    i added in both files with code

---
+++ firebase-test/firebase-test.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCnuHGT34ttHnWVpggi_ZelpUgjWYnFlEU",
+    authDomain: "cool-things-666fa.firebaseapp.com",
+    databaseURL: "https://cool-things-666fa.firebaseio.com",
+    projectId: "cool-things-666fa",
+    storageBucket: "cool-things-666fa.appspot.com",
+    messagingSenderId: "1038438303212"
+  };
+  firebase.initializeApp(config);
+</script>
