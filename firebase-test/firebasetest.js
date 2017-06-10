82ce21fd6d919bf765bf46998444316297f70736
Amy Baldwin <abaldwin008@gmail.com>
2017-06-10 12:28:24 -0700

    Initial commit Firebase App

---
+++ firebase-test/firebasetest.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBv6BMaXsgtYNmdyZZmZHTfh06bHwoF5pc",
+    authDomain: "fir-test-546b1.firebaseapp.com",
+    databaseURL: "https://fir-test-546b1.firebaseio.com",
+    projectId: "fir-test-546b1",
+    storageBucket: "fir-test-546b1.appspot.com",
+    messagingSenderId: "194269179869"
+  };
+  firebase.initializeApp(config);
+</script>
