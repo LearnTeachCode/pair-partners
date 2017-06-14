49a100ef155226264bff1d924ca2e49ded513829
kammitama5 <krystal.maughan@gmail.com>
2017-06-14 15:41:52 +0000

    added logout buttons and event listeners

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -15,6 +15,13 @@ var messageBox = document.getElementById("message");
 // Create a JavaScript object for the HTML element that has id="username"
 var usernameBox = document.getElementById("username");
 
+// Create Objects for Buttons UserInfo
+var userInfoBox = document.getElementById("userinfo");
+// Create Objects for Button Login
+var loginButton = document.getElementById("login");
+// Create Objects for Button Logout
+var logoutButton = document.getElementById("logout");
+
 // Get a reference to the root of our database
 var dbRef = firebase.database().ref();
 // Get a reference to the "greeting" section of our database
@@ -22,6 +29,12 @@ var dbGreeting = dbRef.child("greeting");
 // Get a reference to the "myname" section of our database
 var dbUsername = dbRef.child("myname");
 
+// button function and event listener
+document.addEventListener("click", myButton);
+
+function myButton(github){
+	alert("hi");
+}
 // Whenever "greeting" value in our database is updated, show the data inside messageBox!
 dbGreeting.on("value", function(dataSnapshot) {
 	messageBox.textContent = dataSnapshot.val();
@@ -32,4 +45,14 @@ dbGreeting.on("value", function(dataSnapshot) {
 dbUsername.on("value", function(dataSnapshot) {
 	usernameBox.textContent = dataSnapshot.val();
 	console.log( dataSnapshot.val() );
+});
+
+// Event Listener for Login
+loginButton.addEventListener("click", function(){
+	console.log("User clicked LOGIN");
+});
+
+// Event Listener for Logout
+logoutButton.addEventListener("click", function(){
+	console.log("User clicked LOGOUT");	
 });
