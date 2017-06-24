649f069b907800a13357e9187f3fda22981ce59e
naoever <naoever@users.noreply.github.com>
2017-06-24 15:19:14 -0700

    Update firebasetest.js

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -1,14 +1,13 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCdoBkKYmhVlgvy46sBKLLknJUszf-TYAc",
-  authDomain: "test-web-app-4fde9.firebaseapp.com",
-  databaseURL: "https://test-web-app-4fde9.firebaseio.com",
-  projectId: "test-web-app-4fde9",
-  storageBucket: "test-web-app-4fde9.appspot.com",
-  messagingSenderId: "406177608219"
-};
-
-firebase.initializeApp(config);
+    apiKey: "AIzaSyAAKWAWX9IYQjC1q7hjZL5nsTqWfEulU9I",
+    authDomain: "fir-test-5f0d3.firebaseapp.com",
+    databaseURL: "https://fir-test-5f0d3.firebaseio.com",
+    projectId: "fir-test-5f0d3",
+    storageBucket: "fir-test-5f0d3.appspot.com",
+    messagingSenderId: "852596610591"
+  };
+  firebase.initializeApp(config);
 
 var provider = new firebase.auth.GithubAuthProvider();
 
@@ -45,49 +44,53 @@ dbUsername.on("value", function(dataSnapshot) {
 //When user clicks login button:
 loginButton.addEventListener("click", function(){
  console.log("User clicked LOGIN");
+
+  // Use Firebase with GitHub Auth to log in the user
+  firebase.auth().signInWithPopup(provider).catch(function(error) {
+    // Log any errors to the console
+    console.log(error);
+  });
+
 });
 
-// Use Firebase with GitHub Auth to log in the user
-firebase.auth().signInWithPopup(provider).catch(function(error) {
-  // Log any errors to the console
-  console.log(error);
-});
 
 // When user clicks logout button:
 logoutButton.addEventListener("click", function(){
   console.log("User clicked LOGOUT");
-});
+  // Use Firebase with GitHub Auth to log in the user
+  firebase.auth().signOut().catch(function(error) {
+    // Log any errors to the console
+    console.log(error);
 
-// Use Firebase with GitHub Auth to log in the user
-firebase.auth().signOut().catch(function(error) {
-  // Log any errors to the console
-  console.log(error);
+  });
 });
-// challenge 5
-
 
 
 // When user logs in or logs out:
 firebase.auth().onAuthStateChanged(function(user){
   // If user is now logged in:
-  if (user) {
+  if (user) { 
 
     console.log('User successfully logged in to Firebase!');
-
     // HERE: Update the paragraph with ID of "userinfo" to display user's GitHub username and GitHub profile photo!
+    console.log(user.email);
 
+
+  document.getElementById("userinfo").innerHTML = 'The <strong>' + user.email + '</strong> will go here and the' + 'profile photo will go here: <img src="' + user.photoURL + '">';
   // Otherwise, if no user currently logged in:
+
   } else {
 
     console.log('User successfully logged OUT from Firebase!');
+  document.getElementById("userinfo").textContent = 'Not currently logged in.';
+
+
 
     // HERE: Update the paragraph with ID of "userinfo" to display the message "Not currently logged in."
 
   }
 });
 
-//displayName
-//Inherited from firebase.UserInfo#displayName
 
-//photoURL
-// Inherited from firebase.UserInfo#photoURL
+
+
