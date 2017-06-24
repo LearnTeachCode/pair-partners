29f7b8b0a186d5f4652d255fb70e66de8869ffcc
r7uaz0n <rcktzn007@gmail.com>
2017-06-24 13:27:37 -0700

    Display the user's profile information on the web page

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -9,6 +9,7 @@
   };
   
   firebase.initializeApp(config);
+var provider = new firebase.auth.GithubAuthProvider();
 
 // Create a JavaScript object for each HTML element that we need to use"
 var messageBox = document.getElementById("message");
@@ -75,13 +76,24 @@ firebase.auth().onAuthStateChanged(function(user){
     console.log('User successfully logged in to Firebase!');
 
     // HERE: Update the paragraph with ID of "userinfo" to display user's GitHub username and GitHub profile photo!
+        var username = user.email;
+        var photoProfile = user.photoURL;
+        console.log(username, photoProfile);
+        console.log(user);
+        var myStr = 'Hello, <strong> ';
+        myStr += username;
+        myStr += '</strong> Welcome! <img src="';
+        myStr += photoProfile ;
+        myStr += '" alt="GitHubProfilePic" style="width:150px;height:150px;">';
 
+    document.getElementById("userinfo").innerHTML = myStr; //'Hello, <strong> '+ username + '</strong> Welcome! <img src="' + photoProfile + '" alt="GitHubProfilePic" style="width:200px;height:200px;">';
   // Otherwise, if no user currently logged in:
   } else {
-
+  //   document.getElementById("userinfo").innerHTML = 'Goodbye, <strong> '+ username + '</strong> Have a nice day !': <img src="#">;
     console.log('User successfully logged OUT from Firebase!');
 
     // HERE: Update the paragraph with ID of "userinfo" to display the message "Not currently logged in."
+
 
   }
 });
