0fb9dcdea6d309c46970ce6d6802afb1285ba552
r7uaz0n <rcktzn007@gmail.com>
2017-06-24 09:41:15 -0700

    Add Firebase Authentication functions to your JavaScript - working

--- firebase-test/firebasetest.js
+++ firebase-test/firebasetest.js
@@ -7,57 +7,66 @@
     storageBucket: "fir-test-4f28e.appspot.com",
     messagingSenderId: "276366659662"
   };
-
+  
   firebase.initializeApp(config);
-var provider = new firebase.auth.GithubAuthProvider();
 
-// Create a JavaScript object for each HTML element that we need to use:
+// Create a JavaScript object for each HTML element that we need to use"
 var messageBox = document.getElementById("message");
 var usernameBox = document.getElementById("username");
- debugger; 
+
 var userInfoBox = document.getElementById("userinfo");
-var loginButton = document.getElementById("login");
-var logoutButton = document.getElementById("logout");
+var loginButton = document.getElementById("loginbtn");
+var logoutButton = document.getElementById("logoutbtn");
 
-
-// Get a reference to the root of our database
+// Get a reference to the root of our database called "greeting"
 var dbRef = firebase.database().ref();
 // Get a reference to the "greeting" section of our database
 var dbGreeting = dbRef.child("greeting");
 // Get a reference to the "myname" section of our database
 var dbUsername = dbRef.child("myname");
 
-// Whenever "greeting" value in our database is updated, show the data inside messageBox!
-dbGreeting.on("value", function(dataSnapshot) {
-      messageBox.textContent = dataSnapshot.val();
-      console.log( dataSnapshot.val() );
+// Whenever "greeting" value in our database reference is updated, show the data on our web page!
+dbGreeting.on("value", function(dataSnapshot) { 
+  messageBox.textContent = dataSnapshot.val(); 
+  console.log( dataSnapshot.val() );
 });
+
 
 // Whenever "myname" value in our database is updated, show the data inside usernameBox!
-dbUsername.on("value", function(dataSnapshot) {
-      usernameBox.textContent = dataSnapshot.val();
-      console.log( dataSnapshot.val() );
+dbUsername.on("value", function(dataSnapshot) { 
+  usernameBox.textContent = dataSnapshot.val(); 
+  console.log( dataSnapshot.val() );
 });
 
+
 // When user clicks login button:
-loginButton.addEventListener("click", function(){
-// Use Firebase with GitHub Auth to log in the user
-firebase.auth().signInWithRedirect(provider).catch(function(error) {
-  // Log any errors to the console
-  console.log(error);
+loginButton.addEventListener("click", function() {
+  console.log("User clicked LOGIN"); 
   
-  });
+  
+        // Use Firebase with GitHub Auth to log in the user
+      firebase.auth().signInWithRedirect(provider).catch(function(error) {
+       // Log any errors to the console
+       console.log(error);
+      });
+  
+});
 
 
 // When user clicks logout button:
-logoutButton.addEventListener("click", function(){
-// Use Firebase with GitHub Auth to log in the user
-  firebase.auth().signOut().catch(function(error) {
-  // Log any errors to the console
-    console.log(error);
+logoutButton.addEventListener("click", function() {
+  console.log("User clicked LOGOUT");
+  
+  
+      // Use Firebase with GitHub Auth to log in the user
+      firebase.auth().signOut().catch(function(error) {
+       // Log any errors to the console
+        console.log(error);
+      }); 
   
 });
-  
+
+
 // When user logs in or logs out:
 firebase.auth().onAuthStateChanged(function(user){
   // If user is now logged in:
@@ -66,7 +75,6 @@ firebase.auth().onAuthStateChanged(function(user){
     console.log('User successfully logged in to Firebase!');
 
     // HERE: Update the paragraph with ID of "userinfo" to display user's GitHub username and GitHub profile photo!
-var username;
 
   // Otherwise, if no user currently logged in:
   } else {
@@ -74,7 +82,6 @@ var username;
     console.log('User successfully logged OUT from Firebase!');
 
     // HERE: Update the paragraph with ID of "userinfo" to display the message "Not currently logged in."
-var profilePhoto;
 
   }
 });
